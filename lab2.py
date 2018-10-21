f3e754c6dd0ba93ba4c9e6016b77516003aac896
joyce20-meet <joyce20@meet.mit.edu>
2018-10-21 12:26:55 -0400

    lab3 is not done

--- lab2.py
+++ lab2.py
@@ -11,3 +11,13 @@ def num_list2 (x):
 num_list2(number_list)
 a=[1,2,3,4]
 b=[2,3,5,6]
+list_1=[1,2,3,4,5,6,8]
+list_2=[2,3,4,5,6,7]
+list_3=[]
+for x in list_1:
+	for y in list_2:
+		if x==y:
+			list_3.append(x)
+print list_3 
+
+		 
---
+++ turtleshapes.py
@@ -0,0 +1,9 @@
+import turtle
+angle=145
+length=120
+for i in range(5):
+	turtle.forward(length)
+	turtle.right(angle)
+	
+turtle.mainloop()
+
