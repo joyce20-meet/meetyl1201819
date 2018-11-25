d02973e87315ee325a5111efae3fb41769014674
joyce20-meet <joyce20@meet.mit.edu>
2018-11-25 12:20:19 -0500

    lab7

--- lab7.py
+++ lab7.py
@@ -17,9 +17,17 @@ def check_collision(ball_1,ball_2):
 	y2 = ball_2.ycor()
 	D = math.sqrt(math.pow((x2-x1),2) + math.pow((y2-y1),2))
 	sum_radius = ball_1.radius + ball_2.radius
+	if sum_radius > D:
+		#ball_1.color("green")
+		#all_2.color("pink")
+		print("change radius")
+	else:
+		print("you are done")
 
-ball_1 = Ball(25,"red",2)
-ball_2 = Ball(22,"blue",2)
-
-#print(ball_1.pos())
+ball_1 = Ball(20,"red",2)
+ball_1.pu()
+ball_1.goto(90,30)
+ball_2 = Ball(25,"blue",2)
+check_collision(ball_1,ball_2)
+print(ball_1.pos())
 turtle.mainloop()
