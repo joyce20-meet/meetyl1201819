0b3a88a12996402e6d329cf852ad683c230ca76f
joyce20-meet <joyce20@meet.mit.edu>
2018-12-02 13:05:56 -0500

    lab7

--- lab7.py
+++ lab7.py
@@ -1,15 +1,38 @@
+
 from turtle import *
 import random
 import turtle
 import math
 class Ball(Turtle):
-	def __init__(self,radius,color,speed):
+	def __init__(self,x,y,dx,dy,radius,color):
 		Turtle.__init__(self)
 		self.shape("circle")
 		self.shapesize(radius/10)
 		self.radius = radius
 		self.color(color)
-		self.speed(speed)
+		self.goto(x,y)
+		self.dx = dx
+		self.dy = dy
+		
+
+	
+	def move(self,width,height):
+		self.width = width
+		self.height = height
+		oldx = self.xcor()
+		oldy = self.ycor()
+		newx = oldx + self.dx
+
+		newy = oldy + self.dy
+		self.goto(newx,newy)
+		if newx > width:
+			newx = -newx
+		if newy > height:
+		 	newy = -newy	
+	
+
+
+
 def check_collision(ball_1,ball_2):
 	x1 = ball_1.xcor()
 	y1 = ball_1.ycor()
@@ -18,16 +41,18 @@ def check_collision(ball_1,ball_2):
 	D = math.sqrt(math.pow((x2-x1),2) + math.pow((y2-y1),2))
 	sum_radius = ball_1.radius + ball_2.radius
 	if sum_radius > D:
-		#ball_1.color("green")
-		#all_2.color("pink")
+		ball_1.color("green")
+		ball_2.color("pink")
 		print("change radius")
+		#ball_1.forward(20)
 	else:
 		print("you are done")
+ball_1 = Ball(100,100,5,4,20,"red")
+ball_2 = Ball(50,50,4,3,25,"blue")
 
-ball_1 = Ball(20,"red",2)
-ball_1.pu()
-ball_1.goto(90,30)
-ball_2 = Ball(25,"blue",2)
-check_collision(ball_1,ball_2)
-print(ball_1.pos())
+while True:
+	ball_1.move(10,10)
+	ball_2.move(10,10)
+	check_collision(ball_1,ball_2)
+
 turtle.mainloop()
